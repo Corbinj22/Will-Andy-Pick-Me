b18ab6182b25eb70e6122803fa29b83704c4c3a5
Justin Corbin <jcorbin2290@gmail.com>
2019-12-26 14:11:23 -0700

    revision two finalized with full functionality

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
+// global variables
 var userQuestion = document.getElementById('user-question');
 var submitButton = document.querySelector('.submit-answer');
 var answerDisplay = document.querySelector('.answer');
 var questionDisplay = document.querySelector('.user-question-display');
 var clearButton = document.getElementById('clear')
 
+// event listeners for buttons and submit field
 submitButton.addEventListener("click", showAnswer);
 clearButton.addEventListener("click", resetLeftBox);
 userQuestion.addEventListener("keyup", ableBtns);
+
+// starting state of buttons
 submitButton.disabled = true;
 clearButton.disabled = true;
 
+// function ran to enable buttons on keyup.
+function ableBtns() {
+  submitButton.disabled = false;
+  clearButton.disabled = false;
+}
 
-
+// function to show/display user answer and question
 function showAnswer(){
   var answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Don\'t count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
   var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
@@ -22,16 +31,12 @@ function showAnswer(){
   document.getElementById("eight-ball").style.visibility = "hidden";
 }
 
-
-function ableBtns() {
-submitButton.disabled = false;
-clearButton.disabled = false;
-}
-
+// function to reset left box field and set it back to load state.
 function resetLeftBox(){
   userQuestion.value = "";
   document.getElementById("eight-ball").style.visibility = "visible";
   answerDisplay.innerHTML = "";
   questionDisplay.innerHTML = "";
-
+  submitButton.disabled = true;
+  clearButton.disabled = true;
 }
